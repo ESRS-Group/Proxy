3d8c8cf0998350f6d1263e8758503d0f10e68842
Moderocky <admin@moderocky.com>
2025-04-15 12:08:55 +0100

    Add basic functionality. (#2)
    * Add libraries.
    
    * Add README.
    
    * Basic functionality.

--- src/main/java/mx/kenzie/esrs/AccessCheckerTask.java
+++ src/main/java/mx/kenzie/esrs/AccessCheckerTask.java
@@ -1,4 +1,57 @@
 package mx.kenzie.esrs;
 
-public class AccessCheckerTask {
+import mx.kenzie.clockwork.io.IOQueue;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+
+public class AccessCheckerTask implements Runnable {
+
+    static final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    static final ExecutorService executor = Executors.newCachedThreadPool();
+    static final IOQueue queue = new IOQueue();
+    private static final int TIME_OUT = 300;
+
+
+    protected final InetAddress address;
+    protected final BiConsumer<Boolean, InetAddress> resultTask;
+
+    public static void scheduleCheckAccess(InetAddress address, int frequency, BiConsumer<Boolean, InetAddress> resultTask) {
+        scheduler.scheduleAtFixedRate(new AccessCheckerTask(address, resultTask), frequency, frequency, TimeUnit.SECONDS);
+    }
+
+    public AccessCheckerTask(InetAddress address, BiConsumer<Boolean, InetAddress> resultTask) {
+        this.address = address;
+        this.resultTask = resultTask;
+    }
+
+    @Override
+    public void run() {
+        boolean checked = this.checkRouteAccessible();
+
+        queue.queue(() -> this.resultTask.accept(checked, address));
+    }
+
+    private boolean checkRouteAccessible() {
+        return ping(address.getHostAddress(), 443)
+            || ping(address.getHostAddress(), 80);
+    }
+
+    private static boolean ping(String host, int port) {
+        new InetSocketAddress(host, port);
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), AccessCheckerTask.TIME_OUT);
+            return true;
+        } catch (IOException _) {
+            return false;
+        }
+    }
+
 }
--- src/main/java/mx/kenzie/esrs/Main.java
+++ src/main/java/mx/kenzie/esrs/Main.java
@@ -1,4 +1,35 @@
 package mx.kenzie.esrs;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.function.BiConsumer;
+
 public class Main {
+
+    static final HazyQueue backends = new HazyQueue();
+
+    public static void main(String... args) {
+
+    }
+
+    public static void start(String[] backendAddresses) {
+        for (String backendAddress : backendAddresses) {
+            AccessCheckerTask.scheduleCheckAccess(InetAddress.getLoopbackAddress(), 200,
+                addToProxy(new InetSocketAddress(backendAddress, 443).getAddress()));
+        }
+    }
+
+    private static URI toURI(InetAddress address) {
+        return URI.create("https://" + address.getHostAddress() + "/api/");
+    }
+
+    private static BiConsumer<Boolean, InetAddress> addToProxy(InetAddress address) {
+        return (z, endpoint) -> {
+            URI uri = toURI(endpoint);
+            backends.remove(uri);
+            if (z) backends.pushAny(uri);
+        };
+    }
+
 }
